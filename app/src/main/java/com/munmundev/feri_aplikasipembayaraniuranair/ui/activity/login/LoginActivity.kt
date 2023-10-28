189d0296e39d391bbd6882b0b76d525f62cc4e4c
MunMunDev <mismundev@gmail.com>
2023-10-28 23:43:57 +0800

    add activity login for move activity from splash screen to login and main

---
+++ app/src/main/java/com/munmundev/feri_aplikasipembayaraniuranair/ui/activity/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.munmundev.feri_aplikasipembayaraniuranair.ui.activity.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.munmundev.feri_aplikasipembayaraniuranair.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
