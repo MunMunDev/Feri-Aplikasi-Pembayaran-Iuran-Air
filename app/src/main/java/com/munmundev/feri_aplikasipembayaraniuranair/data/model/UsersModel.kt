6d479eadd8562d897b9ebd6bc9dea31785c05855
MunMunDev <mismundev@gmail.com>
2023-10-29 00:08:46 +0800

    add model. UsersModel for users and ResponseModel for response post API

---
+++ app/src/main/java/com/munmundev/feri_aplikasipembayaraniuranair/data/model/UsersModel.kt
@@ -0,0 +1,26 @@
+package com.munmundev.feri_aplikasipembayaraniuranair.data.model
+
+import com.google.gson.annotations.SerializedName
+
+class UsersModel(
+    @SerializedName("idUser")
+    var idUser: String? = null,
+
+    @SerializedName("nama")
+    var nama: String? = null,
+
+    @SerializedName("alamat")
+    var alamat: String? = null,
+
+    @SerializedName("nomorHp")
+    var nomorHp: String? = null,
+
+    @SerializedName("username")
+    var username: String? = null,
+
+    @SerializedName("password")
+    var password: String? = null,
+
+    @SerializedName("sebagai")
+    var sebagai: String? = null
+)
