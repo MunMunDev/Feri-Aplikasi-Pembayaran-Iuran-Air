edc2bab44acb0829f31f4159e1696128cbbd4641
MunMunDev <mismundev@gmail.com>
2023-10-28 22:51:28 +0800

    update compileSdk and targetSdk from 33 to 34. and update core-ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.munmundev.feri_aplikasipembayaraniuranair"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.munmundev.feri_aplikasipembayaraniuranair"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -44,7 +44,7 @@ dependencies {
     val retrofitVersion = "2.9.0"
     val coroutinesVersion = "1.6.4"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
