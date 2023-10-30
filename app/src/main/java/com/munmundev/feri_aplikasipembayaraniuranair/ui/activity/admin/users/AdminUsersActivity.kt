ea528063da1607faeb49bd5ba96e486494125b0a
MunMunDev <mismundev@gmail.com>
2023-10-30 09:44:27 +0800

    add component for admin

---
+++ app/src/main/java/com/munmundev/feri_aplikasipembayaraniuranair/ui/activity/admin/users/AdminUsersActivity.kt
@@ -0,0 +1,14 @@
+package com.munmundev.feri_aplikasipembayaraniuranair.ui.activity.admin.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.munmundev.feri_aplikasipembayaraniuranair.R
+
+class AdminUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_users)
+
+
+    }
+}
